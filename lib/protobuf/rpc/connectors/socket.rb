09f649773a563c42508cf5939e486c8b52504272
= <brandonsdewitt@gmail.com>
2011-12-09 18:52:36 -0700

    add debug logging to socket connector class

--- lib/protobuf/rpc/connectors/socket.rb
+++ lib/protobuf/rpc/connectors/socket.rb
@@ -18,20 +18,28 @@ module Protobuf
         private
 
         def check_async
-          raise "Cannot use Socket client in async mode" if async?
+          if async?
+            log_error "[client-#{self.class}] Cannot run in async mode"
+            raise "Cannot use Socket client in async mode" 
+          else
+            log_debug "[client-#{self.class}] Async check passed" 
+         end
         end
 
         def close_connection
           @socket.close
+          log_debug "[client-#{self.class}] Connector closed" 
         end
 
         def connect_to_rpc_server
           @socket = TCPSocket.new(options[:host], options[:port])
+          log_debug "[client-#{self.class}] Connection established #{options[:host]}:#{options[:port]}" 
         end
 
         # Method to determine error state, must be used with Connector api
         def error?
           @socket.closed?
+          log_debug "[client-#{self.class}] Error state : #{@socket.closed?}" 
         end
 
         def read_data
@@ -53,6 +61,7 @@ module Protobuf
         def send_data(data)
           @socket.write(data)
           @socket.close_write
+          log_debug "[client-#{self.class}] Write closed." 
         end
 
       end
