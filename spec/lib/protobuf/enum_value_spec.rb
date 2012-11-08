b49855b4da63d42198c47e6041a3f4d7911e3189
Brandon Dewitt <brandonsdewitt@gmail.com>
2012-11-07 18:40:25 -0700

    make Protobuf::EnumValue look like a Fixnum so arel visitor will treat it as a Fixnum

--- spec/lib/protobuf/enum_value_spec.rb
+++ spec/lib/protobuf/enum_value_spec.rb
@@ -3,11 +3,12 @@ require 'spec_helper'
 describe Protobuf::EnumValue do
 
 	subject { Test::EnumTestType::ONE }
+  its(:class) { should eq Fixnum }
 	its(:parent_class) { should eq Test::EnumTestType }
 	its(:name) { should eq :ONE }
 	its(:value) { should eq 1 }
 	its(:to_hash_value) { should eq 1 }
-	its(:to_s) { should eq "ONE" }
+	its(:to_s) { should eq "1" }
 	its(:inspect) { should eq '#<Protobuf::EnumValue Test::EnumTestType::ONE=1>' }
 
 end
