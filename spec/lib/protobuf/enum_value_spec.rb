77b8a25c495fdb6418bf67215e9718f5bbba8064
BJ Neilsen <bj.neilsen@gmail.com>
2012-12-18 10:19:40 -0700

    Re-implement EnumValue#try to fix delegation issue

--- spec/lib/protobuf/enum_value_spec.rb
+++ spec/lib/protobuf/enum_value_spec.rb
@@ -12,9 +12,18 @@ describe Protobuf::EnumValue do
 	its(:inspect) { should eq('#<Protobuf::EnumValue Test::EnumTestType::ONE=1>') }
   specify { subject.to_s(:name).should eq("ONE") }
 
-  it "can be used as the index to an array" do 
+  it "can be used as the index to an array" do
     array = [0, 1, 2, 3]
     array[::Test::EnumTestType::ONE].should eq(1)
+  end
+
+  describe '#try' do
+    specify { subject.try(:class).should eq subject.class }
+    specify { subject.try(:name).should eq subject.name }
+    specify { subject.try(:value).should eq subject.value }
+    specify { subject.try(:to_i).should eq subject.to_i }
+    specify { subject.try(:to_int).should eq subject.to_int }
+    specify { subject.try { |yielded| yielded.should eq subject } }
   end
 
 end
