6d1ecb478f284ed71ddaa4ffeaf26ffdf6552882
BJ Neilsen <bj.neilsen@gmail.com>
2013-03-08 16:16:55 -0700

    Compile Extension Fields to correct message class
    The libprotoc Generator provides extension fields
    to a FileDescriptor or Descriptor object. I
    misunderstood that the descriptor extension fields
    are not the fields that belong to the corresponding
    message proto/class.
    
    The fix is to store the extension fields by their
    containing type name when we initially parse
    through the messages in the file descriptor.
    Then when we  go to print each message's fields
    we print out the previously extracted
    extension fields.
    
    Fixes #69.

--- spec/support/test/enum.pb.rb
+++ spec/support/test/enum.pb.rb
@@ -3,24 +3,25 @@
 #
 require 'protobuf/message'
 
+##
+# Imports
+#
+require 'test/resource.pb'
+
 module Test
   ##
   # Enum Classes
   #
-  class EnumTestType < ::Protobuf::Enum; end
+  class EnumTestType < ::Protobuf::Enum
+    define :ONE, 1
+    define :TWO, 2
+  end
+  
   
   ##
   # Message Classes
   #
   class EnumTestMessage < ::Protobuf::Message; end
-  
-  ##
-  # Enum Values
-  #
-  class EnumTestType
-    define :ONE, 1
-    define :TWO, 2
-  end
   
   ##
   # Message Fields
@@ -29,6 +30,13 @@ module Test
     optional ::Test::EnumTestType, :non_default_enum, 1
     optional ::Test::EnumTestType, :default_enum, 2, :default => ::Test::EnumTestType::ONE
     repeated ::Test::EnumTestType, :repeated_enums, 3
+  end
+  
+  ##
+  # Extended Messages
+  #
+  class ::Test::Resource
+    optional ::Protobuf::Field::Int32Field, :ext_other_file_defined_field, 200, :extension => true
   end
   
 end
