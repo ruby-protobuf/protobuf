985d567f1b5582239972e2d8dbc60653498fb8da
Devin Christensen <quixoten@gmail.com>
2013-06-19 13:31:02 -0600

    Fix spec

--- spec/functional/zmq_server_spec.rb
+++ spec/functional/zmq_server_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+
 require 'spec/support/test/resource_service'
 require 'protobuf/rpc/service_directory'
 
@@ -13,11 +14,13 @@ describe 'Functional ZMQ Client' do
   }}
 
   let(:server) { ::Protobuf::Rpc::Zmq::Server.new(options) }
-  let(:server_thread) { Thread.new(server) { |server| server.run } }
+  let!(:server_thread) { Thread.new(server) { |server| server.run } }
+
+  before(:all) do
+    load "protobuf/zmq.rb"
+  end
 
   before do
-    load "protobuf/zmq.rb"
-    server_thread.abort_on_exception = true
     Thread.pass until server.running?
   end
 
@@ -46,14 +49,12 @@ describe 'Functional ZMQ Client' do
   context 'when a message is malformed' do
     it 'calls the on_failure callback' do
       error = nil
-      StubServer.new(:server => Protobuf::Rpc::Zmq::Server) do
-        request = ::Test::ResourceFindRequest.new(:active => true)
-        client = ::Test::ResourceService.client
+      request = ::Test::ResourceFindRequest.new(:active => true)
+      client = ::Test::ResourceService.client
 
-        client.find(request) do |c|
-          c.on_success { raise "shouldn't pass" }
-          c.on_failure {|e| error = e }
-        end
+      client.find(request) do |c|
+        c.on_success { raise "shouldn't pass" }
+        c.on_failure {|e| error = e }
       end
       error.message.should match(/name.*required/)
     end
@@ -62,14 +63,12 @@ describe 'Functional ZMQ Client' do
   context 'when the request type is wrong' do
     it 'calls the on_failure callback' do
       error = nil
-      StubServer.new(:server => Protobuf::Rpc::Zmq::Server) do
-        request = ::Test::Resource.new(:name => 'Test Name')
-        client = ::Test::ResourceService.client
+      request = ::Test::Resource.new(:name => 'Test Name')
+      client = ::Test::ResourceService.client
 
-        client.find(request) do |c|
-          c.on_success { raise "shouldn't pass" }
-          c.on_failure {|e| error = e}
-        end
+      client.find(request) do |c|
+        c.on_success { raise "shouldn't pass" }
+        c.on_failure {|e| error = e}
       end
       error.message.should match(/expected request.*ResourceFindRequest.*Resource instead/i)
     end
@@ -78,13 +77,11 @@ describe 'Functional ZMQ Client' do
   context 'when the server takes too long to respond' do
     it 'responds with a timeout error' do
       error = nil
-      StubServer.new(:server => Protobuf::Rpc::Zmq::Server) do
-        client = ::Test::ResourceService.client(:timeout => 1)
+      client = ::Test::ResourceService.client(:timeout => 1)
 
-        client.find_with_sleep(:sleep => 2) do |c|
-          c.on_success { raise "shouldn't pass" }
-          c.on_failure { |e| error = e }
-        end
+      client.find_with_sleep(:sleep => 2) do |c|
+        c.on_success { raise "shouldn't pass" }
+        c.on_failure { |e| error = e }
       end
       error.message.should match(/The server took longer than 1 seconds to respond/i)
     end
