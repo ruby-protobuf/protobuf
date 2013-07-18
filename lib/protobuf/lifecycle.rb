aa46d0e691a43e8bd16e53479db6348b4f47847f
Brandon Dewitt <brandonsdewitt@gmail.com>
2013-07-18 15:50:36 -0600

    add lifecycle handler and call registered blocks after_server_bind when running, will fill in more lifecycle events as we continue to build up the server

---
+++ lib/protobuf/lifecycle.rb
@@ -0,0 +1,33 @@
+module Protobuf
+  class Lifecycle
+
+    def self.register( event_name, &blk )
+      event_name = normalized_event_name( event_name )
+
+      lifecycle_events[ event_name ] ||= []
+      lifecycle_events[ event_name ] << blk
+    end
+
+    def self.trigger( event_name )
+      event_name = normalized_event_name( event_name )
+
+      if lifecycle_events.has_key?( event_name )
+        lifecycle_events[ event_name ].each do |block|
+          block.call
+        end
+      end
+    end
+    
+    def self.normalized_event_name( event_name )
+      return "#{event_name}".downcase
+    end
+
+    class << self
+      attr_accessor :lifecycle_events
+
+      alias_method :on, :register
+    end
+
+    @lifecycle_events ||= {}
+  end
+end
