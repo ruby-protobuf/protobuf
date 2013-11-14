ff310c81e9cf60ea09f8b71e8a4c09d619864a40
BJ Neilsen <bj.neilsen@gmail.com>
2013-11-14 13:42:56 -0700

    First cut at an Enum API refactor
    == Aliases
    
    Extend Optionable module to get class-level option management. This
    allows us to support the allow_alias protobuf option in libprotoc
    v2.5.0.
    
    Adds methods to return an array of EnumValue objects corresponding to a
    given number "value".
    
    == "Value"
    
    The term "value" is used to mean multiple things in the Enum
    implementation: 1) an EnumValue object, and 2) an integer commonly
    called a tag number.
    
    This cut attempts to use the term "number" for arguments when the arg
    should be number-like, and use the term `enum_value` in methods and
    comments when we are referencing the actual object type. All method
    name changes are aliased to their old names.
    
    Ideally EnumValue would not be separate from Enum. I'm not willing to
    make that cut here, possibly in a future API refactor cut.

--- lib/protobuf/enum.rb
+++ lib/protobuf/enum.rb
@@ -1,62 +1,207 @@
 require 'protobuf/enum_value'
+require 'protobuf/optionable'
 
 module Protobuf
   class Enum
+    # Public: Allows setting Options on the Enum class.
+    include ::Protobuf::Optionable
 
-    def self.define(name, value)
-      enum_value = ::Protobuf::EnumValue.new(self, name, value)
-      const_set(name, enum_value)
-      @values ||= {}
-      @names ||= {}
-      @values[name] = enum_value
-      @names[value] = name
+    def self.aliases_allowed?
+      self.get_option(:allow_alias)
     end
 
-    # Fetch the given enum by a variety of type-checking
-    # mechanisms. This is useful for the enum field setters
-    # as well as repeated enum field construction.
+    # Public: Get an array of EnumValue objects with the given value.
+    #
+    # number - An object that responds to to_i.
+    #
+    # Returns an array with zero or more EnumValue objects or nil.
+    #
+    def self.all_enum_values_by_value(number)
+      self.enum_values.values.select do |enum_value|
+        enum_value.to_i == number.to_i
+      end
+    end
+
+    # Public: Get all integer tag values defined by this enum.
+    #
+    # Examples
+    #
+    #   class StateMachine < ::Protobuf::Enum
+    #     set_option :allow_alias
+    #     define :ON, 1
+    #     define :STARTED, 1
+    #     define :OFF, 2
+    #   end
+    #
+    #   StateMachine.all_values #=> [ 1, 2 ]
+    #
+    # Returns an array of unique integers.
+    #
+    def self.all_values
+      self.enum_values.values.map(&:to_i).uniq
+    end
+
+    # Internal: DSL method to define a protobuf EnumValue. The given name will
+    # become a constant for this Enum which points to an EnumValue object.
+    #
+    # Examples
+    #
+    #   class StateMachine < ::Protobuf::Enum
+    #     define :ON, 1
+    #     define :OFF, 2
+    #   end
+    #   StateMachine::ON  #=> #<StateMachine::ON=1>
+    #   StateMachine::OFF #=> #<StateMachine::OFF=2>
+    #
+    # Returns nothing.
+    #
+    def self.define(name, value)
+      enum_value = ::Protobuf::EnumValue.new(self, name, value)
+      @enum_values ||= {}
+      @enum_values[name] = enum_value
+      const_set(name, enum_value)
+    end
+
+    # Public: Get the EnumValue associated with the given name.
+    #
+    # name - An object that responds to to_sym. Case-sensitive.
+    #
+    # Example
+    #
+    #   class StateMachine < ::Protobuf::Enum
+    #     define :ON, 1
+    #     define :OFF, 2
+    #   end
+    #
+    #   StateMachine.enum_value_by_name(:ON)  # => #<StateMachine::ON=1>
+    #   StateMachine.enum_value_by_name("ON") # => #<StateMachine::ON=1>
+    #   StateMachine.enum_value_by_name(:on)  # => nil
+    #   StateMachine.enum_value_by_name(:FOO) # => nil
+    #
+    # Returns the EnumValue object with the given name. `nil` will be returned
+    # if the given name does not have a correlating value.
+    #
+    def self.enum_value_by_name(name)
+      self.const_get(name.to_sym)
+    rescue NameError, NoMethodError
+      nil
+    end
+
+    # Public: Get the EnumValue object corresponding to the given number.
+    #
+    # number - An object that responds to to_i.
+    #
+    # Returns an EnumValue object or nil. If the number corresponds to multiple
+    #   EnumValue objects an array of EnumValue objects will be returned.
+    #
+    def self.enum_value_by_value(number)
+      enum_value_by_name(name_by_value(number))
+    end
+
+    # Public: The internal values hash.
+    #
+    def self.enum_values
+      @enum_values
+    end
+
+    # Public: Get an EnumValue by a variety of type-checking mechanisms.
+    #
+    # value - An EnumValue, Numeric, String, or Symbol object.
+    #
+    # Example
+    #
+    #   class StateMachine < ::Protobuf::Enum
+    #     set_option :allow_alias
+    #     define :ON, 1
+    #     define :STARTED, 1
+    #     define :OFF, 2
+    #   end
+    #
+    #   StateMachine.fetch(StateMachine::ON)  # => #<StateMachine::ON=1>
+    #   StateMachine.fetch(:ON)               # => #<StateMachine::ON=1>
+    #   StateMachine.fetch("STARTED")         # => #<StateMachine::STARTED=1>
+    #   StateMachine.fetch(1)                 # => [ #<StateMachine::ON=1>, #<StateMachine::STARTED=1> ]
+    #
+    # Returns an EnumValue object or nil.
+    #
     def self.fetch(value)
       case value
       when ::Protobuf::EnumValue then
         value
       when Numeric then
-        enum_by_value(value.to_i)
+        enum_value_by_value(value.to_i)
       when String, Symbol then
-        value_by_name(value.to_sym)
+        enum_value_by_name(value.to_sym)
       else
         nil
       end
     end
 
-    def self.enum_by_value(value)
-      value_by_name(name_by_value(value))
+    # Public: Get the Symbol name associated with the given number.
+    #
+    # number - An object that responds to to_i.
+    #
+    # Examples
+    #
+    #   # Without aliases
+    #   class StateMachine < ::Protobuf::Enum
+    #     define :ON, 1
+    #     define :OFF, 2
+    #   end
+    #
+    #   StateMachine.name_by_value(1) # => :ON
+    #   StateMachine.name_by_value(2) # => :OFF
+    #   StateMachine.name_by_value(3) # => nil
+    #
+    #   # With aliases
+    #   class StateMachine < ::Protobuf::Enum
+    #     set_option :allow_alias
+    #     define :STARTED, 1
+    #     define :ON, 1
+    #     define :OFF, 2
+    #   end
+    #
+    #   StateMachine.name_by_value(1) # => :STARTED
+    #   StateMachine.name_by_value(2) # => :OFF
+    #
+    # Returns the symbol name of the enum constant given it's associated value.
+    #   If the given value has multiple names associated (due to allow_alias)
+    #   the first defined name will be returned. `nil` will be returned when
+    #   the given value does not have a correlating name.
+    #
+    def self.name_by_value(number)
+      self.enum_values.values.detect { |enum_value|
+        enum_value.to_i == number.to_i
+      }.try(:name)
     end
 
-    def self.integer_values
-      self.values.values.map(&:to_i)
-    end
-
-    def self.name_by_value(value)
-      (!value.nil? && value.respond_to?(:to_i)) ? @names[value.to_i] : nil
-    end
-
-    def self.valid_tag?(tag)
-      !! name_by_value(tag)
-    end
-
-    def self.value_by_name(name)
-      @values[name]
-    end
-
-    def self.values
-      @values
+    # Public: Check if the given tag value is defined by this Enum.
+    # See name_by_value.
+    #
+    # number - An object that responds to to_i.
+    #
+    # Returns a boolean.
+    #
+    def self.valid_value?(number)
+      !! name_by_value(number)
     end
 
     ##
     # Class Aliases
     #
     class << self
+      # Deprecated:
+      alias_method :valid_tag?, :valid_value?
+
+      # Deprecated:
+      alias_method :values, :enum_values
+
+      # Deprecated: Alias of name_by_value.
       alias_method :get_name_by_tag, :name_by_value
+
+      # Deprecated: Alias of enum_value_by_value.
+      alias_method :value_by_name, :enum_value_by_value
     end
+
   end
 end
--- spec/lib/protobuf/enum_spec.rb
+++ spec/lib/protobuf/enum_spec.rb
@@ -9,103 +9,176 @@ describe Protobuf::Enum do
     Test::EnumTestType.define(:THREE, 3)
   end
 
+  before(:all) do
+    EnumAliasTest = ::Class.new(::Protobuf::Enum) do
+      set_option :allow_alias
+      define :FOO, 1
+      define :BAR, 1
+      define :BAZ, 2
+    end
+  end
+
+  describe '.aliases_allowed?' do
+    it 'is false when the option is not set' do
+      expect(Test::EnumTestType.aliases_allowed?).to be_false
+    end
+  end
+
+  describe '.all_enum_values_by_value' do
+    it 'returns an array of EnumValues for the given tag' do
+      expect(EnumAliasTest.all_enum_values_by_value(1)).to eq([ EnumAliasTest::FOO, EnumAliasTest::BAR ])
+      expect(EnumAliasTest.all_enum_values_by_value(2)).to eq([ EnumAliasTest::BAZ ])
+      expect(EnumAliasTest.all_enum_values_by_value(3)).to eq([])
+    end
+  end
+
   describe '.define' do
     it 'defines a constant enum value on the parent class' do
-      Test::EnumTestType.constants.should include(name)
-      Test::EnumTestType::THREE.should be_a(Protobuf::EnumValue)
+      expect(Test::EnumTestType.constants).to include(name)
+      expect(Test::EnumTestType::THREE).to be_a(Protobuf::EnumValue)
+    end
+
+    context 'when enum allows aliases' do
+      before(:all) do
+        DefineEnumAlias = ::Class.new(::Protobuf::Enum) do
+          set_option :allow_alias
+        end
+      end
+
+      it 'allows defining enum values with the same tag number' do
+        expect {
+          DefineEnumAlias.define(:FOO, 1)
+          DefineEnumAlias.define(:BAR, 1)
+        }.not_to raise_error
+      end
     end
   end
 
   describe '.fetch' do
     context 'when value is an EnumValue' do
       it 'responds with that object' do
-        Test::EnumTestType.fetch(Test::EnumTestType::THREE).name.should eq Test::EnumTestType::THREE.name
+        expect(Test::EnumTestType.fetch(Test::EnumTestType::THREE)).to eq(Test::EnumTestType::THREE)
       end
     end
 
     context 'when value can be coerced to a symbol' do
       it 'fetches based on the symbol name value' do
-        Test::EnumTestType.fetch("ONE").should eq Test::EnumTestType::ONE
-        Test::EnumTestType.fetch(:ONE).should eq Test::EnumTestType::ONE
+        expect(Test::EnumTestType.fetch("ONE")).to eq(Test::EnumTestType::ONE)
+        expect(Test::EnumTestType.fetch(:ONE)).to eq(Test::EnumTestType::ONE)
       end
     end
 
     context 'when value can be coerced to an integer' do
       it 'fetches based on the integer tag value' do
-        Test::EnumTestType.fetch(3.0).should eq Test::EnumTestType::THREE
-        Test::EnumTestType.fetch(3).should eq Test::EnumTestType::THREE
+        expect(Test::EnumTestType.fetch(3.0)).to eq(Test::EnumTestType::THREE)
+        expect(Test::EnumTestType.fetch(3)).to eq(Test::EnumTestType::THREE)
+      end
+
+      context 'when enum allows aliases' do
+        it 'fetches the first defined EnumValue' do
+          expect(EnumAliasTest.fetch(1)).to eq(EnumAliasTest::FOO)
+        end
       end
     end
 
     context 'when value is not an applicable type' do
       it 'returns a nil' do
-        Test::EnumTestType.fetch(Test::Resource.new).should be_nil
-        Test::EnumTestType.fetch(nil).should be_nil
-        Test::EnumTestType.fetch(false).should be_nil
+        expect(Test::EnumTestType.fetch(Test::Resource.new)).to be_nil
+        expect(Test::EnumTestType.fetch(nil)).to be_nil
+        expect(Test::EnumTestType.fetch(false)).to be_nil
       end
     end
   end
 
-  describe '.enum_by_value' do
+  describe '.enum_value_by_value' do
     it 'gets the EnumValue corresponding to the given value (tag)' do
-      Test::EnumTestType.enum_by_value(tag).should eq Test::EnumTestType.const_get(name)
+      expect(Test::EnumTestType.enum_value_by_value(tag)).to eq(Test::EnumTestType.const_get(name))
     end
   end
 
   describe '.name_by_value' do
     it 'get the name by value of the enum given the enum' do
-      Test::EnumTestType.name_by_value(::Test::EnumTestType::THREE).should eq name
+      expect(Test::EnumTestType.name_by_value(::Test::EnumTestType::THREE)).to eq(name)
     end
 
     it 'gets the name of the enum corresponding to the given value (tag)' do
-      Test::EnumTestType.name_by_value(tag).should eq name
+      expect(Test::EnumTestType.name_by_value(tag)).to eq(name)
+    end
+
+    it 'gets the name when the tag is coercable to an int' do
+      expect(Test::EnumTestType.name_by_value("3")).to eq(name)
+    end
+
+    it 'returns nil when tag does not correspond to a name' do
+      expect(Test::EnumTestType.name_by_value(12345)).to be_nil
     end
 
     context 'when given name is nil' do
       it 'returns a nil' do
-        Test::EnumTestType.name_by_value(nil).should be_nil
+        expect(Test::EnumTestType.name_by_value(nil)).to be_nil
+      end
+    end
+
+    context 'when enum allows aliases' do
+      it 'returns the first defined name for that value' do
+        expect(EnumAliasTest.name_by_value(1)).to eq(:FOO)
       end
     end
   end
 
-  describe '.valid_tag?' do
+  describe '.valid_value?' do
     context 'when tag is defined' do
-      specify { Test::EnumTestType.valid_tag?(tag).should be_true }
+      specify { expect(Test::EnumTestType.valid_value?(tag)).to be_true }
     end
 
     context 'when tag is not defined' do
-      specify { Test::EnumTestType.valid_tag?(300).should be_false }
+      specify { expect(Test::EnumTestType.valid_value?(300)).to be_false }
     end
-  end
 
-  describe '.value_by_name' do
-    it 'gets the EnumValue corresponding to the given name' do
-      Test::EnumTestType.value_by_name(name).should eq Test::EnumTestType::THREE
-    end
-  end
-
-  describe '.values' do
-    it 'provides a hash of defined EnumValues' do
-      Test::EnumTestType.values.should eq({
-        :MINUS_ONE => Test::EnumTestType::MINUS_ONE,
-        :ONE   => Test::EnumTestType::ONE,
-        :TWO   => Test::EnumTestType::TWO,
-        :THREE => Test::EnumTestType::THREE
-      })
-    end
-  end
-
-  describe '.integer_values' do
-    before do
-      IntegerValuesEnumTest = ::Class.new(::Protobuf::Enum) do
-        define :FOO, 1
-        define :BAR, 2
-        define :BAZ, 5
+    context 'when enum allows aliases' do
+      it 'treats aliased values as valid' do
+        expect(EnumAliasTest.valid_value?(1)).to be_true
       end
     end
+  end
 
+  describe '.enum_value_by_name' do
+    it 'gets the EnumValue corresponding to the given name' do
+      expect(Test::EnumTestType.enum_value_by_name(name)).to eq(Test::EnumTestType::THREE)
+    end
+  end
+
+  describe '.enum_values' do
+    it 'provides a hash of defined EnumValue objects' do
+      expect(Test::EnumTestType.values).to eq({
+        :MINUS_ONE => Test::EnumTestType::MINUS_ONE,
+        :ONE       => Test::EnumTestType::ONE,
+        :TWO       => Test::EnumTestType::TWO,
+        :THREE     => Test::EnumTestType::THREE
+      })
+    end
+
+    context 'when enum allows aliases' do
+
+      it 'treats aliased values as valid' do
+        expect(EnumAliasTest.enum_values).to eq({
+          :FOO => EnumAliasTest::FOO,
+          :BAR => EnumAliasTest::BAR,
+          :BAZ => EnumAliasTest::BAZ
+        })
+      end
+    end
+  end
+
+  describe '.all_values' do
     it 'provides an array of the integer values' do
-      IntegerValuesEnumTest.integer_values.should eq([ 1, 2, 5 ])
+      expect(Test::EnumTestType.all_values).to include(-1, 1, 2, 3)
+    end
+
+    context 'when enum allows aliases' do
+      it 'returns a unique array of all defined enum values' do
+        expect(EnumAliasTest.all_values).to include(1, 2)
+      end
     end
   end
 
@@ -115,6 +188,7 @@ describe Protobuf::Enum do
   end
 
   context 'when coercing from integer' do
-    specify { 0.should eq(Test::StatusType::PENDING) }
+    specify { expect(0).to eq(Test::StatusType::PENDING) }
   end
+
 end
