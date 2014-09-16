a61a71f19b93f5d2802dba620d3cc0f624727658
Devin Christensen <quixoten@gmail.com>
2014-09-16 09:26:50 -0600

    Remove backend socket from worker

--- lib/protobuf/rpc/servers/zmq/worker.rb
+++ lib/protobuf/rpc/servers/zmq/worker.rb
@@ -15,9 +15,6 @@ module Protobuf
         def initialize(server, broker)
           @server = server
           @broker = broker
-
-          init_zmq_context
-          init_backend_socket
         rescue
           teardown
           raise
@@ -49,8 +46,6 @@ module Protobuf
           end
 
           job_queue << :shutdown # propogate :shutdown to other workers
-        ensure
-          teardown
         end
 
         def job_queue
@@ -59,36 +54,8 @@ module Protobuf
 
         private
 
-        def init_zmq_context
-          if inproc?
-            @zmq_context = @server.zmq_context
-          else
-            @zmq_context = ZMQ::Context.new
-          end
-        end
-
-        def init_backend_socket
-          @backend_socket = @zmq_context.socket(ZMQ::DEALER)
-          zmq_error_check(@backend_socket.connect(@server.backend_uri))
-        end
-
         def inproc?
           !!@server.try(:inproc?)
-        end
-
-        def read_from_backend
-          frames = []
-          zmq_error_check(@backend_socket.recv_strings(frames))
-          frames
-        end
-
-        def teardown
-          @backend_socket.try(:close)
-          @zmq_context.try(:terminate) unless inproc?
-        end
-
-        def write_to_backend(frames)
-          zmq_error_check(@backend_socket.send_strings(frames))
         end
       end
     end
