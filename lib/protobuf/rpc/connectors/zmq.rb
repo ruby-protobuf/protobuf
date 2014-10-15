fbaa7f158947a2e86a51e46d6c4f4750eec047e2
Tamir Duberstein <tamird@squareup.com>
2014-10-15 11:18:15 -0700

    Style/SpaceAroundOperators

--- lib/protobuf/rpc/connectors/zmq.rb
+++ lib/protobuf/rpc/connectors/zmq.rb
@@ -129,7 +129,7 @@ module Protobuf
             port = options[:port]
             return "tcp://#{host}:#{port}" if host_alive?(host)
 
-            sleep(1.0/100.0)
+            sleep(1.0 / 100.0)
           end
 
           raise "Host not found for service #{service}"
