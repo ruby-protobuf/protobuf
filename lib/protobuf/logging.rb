797e86328bc8d4c69126d5a8145d421f7cc405bb
Tamir Duberstein <tamird@squareup.com>
2014-10-15 13:09:51 -0700

    Style/SelfAssignment

--- lib/protobuf/logging.rb
+++ lib/protobuf/logging.rb
@@ -3,8 +3,6 @@ require 'logger'
 module Protobuf
   module Logging
     def self.initialize_logger(log_target = $stdout, log_level = ::Logger::INFO)
-      @counter ||= 0
-      @counter = @counter + 1
       @logger = Logger.new(log_target)
       @logger.level = log_level
       @logger
