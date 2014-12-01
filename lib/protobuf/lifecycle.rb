afa32a9f3dfe5c4f043a7e568a7671dcdf162497
Tamir Duberstein <tamird@squareup.com>
2014-12-01 14:49:44 -0800

    DRY class methods

--- lib/protobuf/lifecycle.rb
+++ lib/protobuf/lifecycle.rb
@@ -1,46 +1,39 @@
 module Protobuf
   class Lifecycle
-    include ::Protobuf::Logging
-
-    def self.register(event_name, &blk)
-      fail "Lifecycle register must have a block" unless block_given?
-      event_name = normalized_event_name(event_name)
-
-      if ::Protobuf.print_deprecation_warnings?
-        $stderr.puts <<-ERROR
-            [DEPRECATED] ::Protobuf::Lifecycle has been deprecated and will be removed in a future version.
-                         Use ::ActiveSupport::Notifications.subscribe('#{event_name}')
-        ERROR
-      end
-
-      ::ActiveSupport::Notifications.subscribe(event_name) do |_name, _start, _finish, _id, args|
-        blk.call(*args)
-      end
-    end
-
-    def self.trigger(event_name, *args)
-      if ::Protobuf.print_deprecation_warnings?
-        $stderr.puts <<-ERROR
-            [DEPRECATED] ::Protobuf::Lifecycle has been deprecated and will be removed in a future version.
-                         Use ::ActiveSupport::Notifications.instrument(...)
-        ERROR
-      end
-
-      event_name = normalized_event_name(event_name)
-
-      ::ActiveSupport::Notifications.instrument(event_name, args)
-    end
-
-    def self.normalized_event_name(event_name)
-      "#{event_name}".downcase
-    end
-
     class << self
-      attr_accessor :lifecycle_events
+      def register(event_name, &blk)
+        fail "Lifecycle register must have a block" unless block_given?
+        event_name = normalized_event_name(event_name)
 
+        if ::Protobuf.print_deprecation_warnings?
+          $stderr.puts <<-ERROR
+          [DEPRECATED] ::Protobuf::Lifecycle has been deprecated and will be removed in a future version.
+            Use ::ActiveSupport::Notifications.subscribe('#{event_name}')
+          ERROR
+        end
+
+        ::ActiveSupport::Notifications.subscribe(event_name) do |_name, _start, _finish, _id, args|
+          blk.call(*args)
+        end
+      end
       alias_method :on, :register
-    end
 
-    @lifecycle_events ||= {}
+      def trigger(event_name, *args)
+        if ::Protobuf.print_deprecation_warnings?
+          $stderr.puts <<-ERROR
+          [DEPRECATED] ::Protobuf::Lifecycle has been deprecated and will be removed in a future version.
+            Use ::ActiveSupport::Notifications.instrument(...)
+          ERROR
+        end
+
+        event_name = normalized_event_name(event_name)
+
+        ::ActiveSupport::Notifications.instrument(event_name, args)
+      end
+
+      def normalized_event_name(event_name)
+        "#{event_name}".downcase
+      end
+    end
   end
 end
