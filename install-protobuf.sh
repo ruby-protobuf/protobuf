06a66fb0d583831689f340e2594d35743ac2598f
Tamir Duberstein <tamird@gmail.com>
2015-03-16 09:32:46 -0700

    Test against protobuf 3

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -2,9 +2,27 @@
 
 set -ex
 
-version=2.6.1
-basename=protobuf-$version
+gdie() {
+    echo "$@" >&2
+    exit 1
+}
 
-curl -sL https://github.com/google/protobuf/releases/download/v$version/$basename.tar.bz2 | tar jx
+test -n "$PROTOBUF_VERSION" || die "PROTOBUF_VERSION env var is undefined"
 
-cd $basename && ./configure --prefix=/usr && make && make install
+case "$PROTOBUF_VERSION" in
+2*)
+    basename=protobuf-$PROTOBUF_VERSION
+    ;;
+3*)
+    basename=protobuf-cpp-$PROTOBUF_VERSION
+    ;;
+*)
+    die "unknown protobuf version: $PROTOBUF_VERSION"
+    ;;
+esac
+
+curl -sL https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/$basename.tar.gz | tar zx
+
+cd protobuf-$PROTOBUF_VERSION
+
+./configure --prefix=/usr && make -j2 && make install
