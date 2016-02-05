35557891c17a149baa799e3c8299d3e3ea06c79d
Dan Jones <dan.jones@moneydesktop.com>
2016-02-05 12:32:17 -0700

    Add @memoized_encoded to setter in message_field.rb

--- lib/protobuf/field/message_field.rb
+++ lib/protobuf/field/message_field.rb
@@ -40,6 +40,7 @@ module Protobuf
         field = self
         message_class.class_eval do
           define_method("#{field.name}=") do |val|
+            @memoized_encoded = nil
             case
             when val.nil?
               @values.delete(field.name)
