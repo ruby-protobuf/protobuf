c13c5fab7eed3f3860456fb5ed72e1130166cba4
Brandon Dewitt <brandonsdewitt@gmail.com>
2016-02-06 13:56:50 -0700

    already loading field on define and should not need to calcualte the tag encoding at runtime

--- lib/protobuf/encoder.rb
+++ lib/protobuf/encoder.rb
@@ -4,20 +4,15 @@ module Protobuf
       message.each_field_for_serialization do |field, value|
         if field.repeated?
           if field.packed?
-            key = (field.tag << 3) | ::Protobuf::WireType::LENGTH_DELIMITED
             packed_value = value.map { |val| field.encode(val) }.join
-            stream << ::Protobuf::Field::VarintField.encode(key)
-            stream << ::Protobuf::Field::VarintField.encode(packed_value.size)
-            stream << packed_value
+            stream << "#{field.tag_encoded}#{::Protobuf::Field::VarintField.encode(packed_value.size)}#{packed_value}"
           else
             value.each do |val|
-              key = (field.tag << 3) | field.wire_type
-              stream << "#{::Protobuf::Field::VarintField.encode(key)}#{field.encode(val)}"
+              stream << "#{field.tag_encoded}#{field.encode(val)}"
             end
           end
         else
-          key = (field.tag << 3) | field.wire_type
-          stream << "#{::Protobuf::Field::VarintField.encode(key)}#{field.encode(value)}"
+          stream << "#{field.tag_encoded}#{field.encode(value)}"
         end
       end
 
