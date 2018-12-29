69d948be6ef01d492b143e90136d6386f2e2951c
Brandon Dewitt <brandonsdewitt@gmail.com>
2018-12-28 17:14:51 -0700

    account for int32 values

--- lib/protobuf/field/int32_field.rb
+++ lib/protobuf/field/int32_field.rb
@@ -7,15 +7,28 @@ module Protobuf
       ##
       # Class Methods
       #
+      if defined?(::ProtobufJavaHelpers)
+        include ::ProtobufJavaHelpers::Varinter
+        include ::ProtobufJavaHelpers::IntegerProtobufField
+        include ::ProtobufJavaHelpers::Int32ProtobufField
+        extend ::ProtobufJavaHelpers::Int32ProtobufField
 
-      def self.max
-        INT32_MAX
+        def encode(value)
+          to_varint_64(value)
+        end
+
+        def decode(value)
+          decode_varint_64(value)
+        end
+      else
+        def self.max
+          INT32_MAX
+        end
+
+        def self.min
+          INT32_MIN
+        end
       end
-
-      def self.min
-        INT32_MIN
-      end
-
     end
   end
 end
