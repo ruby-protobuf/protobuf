e6926cf21723f0e948d7c243328ccf2d8c7e8f3a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-02-03 16:31:59 +0000

    Add failing spec for int64 to JSON

--- spec/lib/protobuf/field/int64_field_spec.rb
+++ spec/lib/protobuf/field/int64_field_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Protobuf::Field::Int64Field do
 
   it_behaves_like :packable_field, described_class
 
+  let(:message) do
+    Class.new(::Protobuf::Message) do
+      optional :int64, :field, 1
+    end
+  end
+
+  # https://developers.google.com/protocol-buffers/docs/proto3#json
+  describe '.{to_json, from_json}' do
+    it 'serialises int64 value as string' do
+      instance = message.new(:field => 10)
+      expect(instance.to_json).to eq('{"field":"10"}')
+    end
+  end
 end
